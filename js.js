7afb93a44d2d48aa99cc3ee43a6f57622e6521d7
Amir <chikchicly@gmil.com>
2025-10-30 12:14:58 +0700

    menu add

--- js.js
+++ js.js
@@ -1,5 +1,36 @@
 window.addEventListener("DOMContentLoaded",main);
 
+function openMenu(){
+    return new Promise((resolve)=>{
+        function menu(){
+            const createMenu=document.createElement("div");
+            createMenu.classList.add("openMenu");
+            createMenu.innerHTML=`<a href="/">Главная</a><a href="/about">О мастере</a><a href="/#services">Услуги</a><a href="/#prices">Цены</a><a href="/#contacts">Контакты</a>`;
+            document.querySelector("body").prepend(createMenu);
+
+            document.querySelector(".nav").addEventListener("click",()=>{
+                console.log('click');
+                document.querySelector(".openMenu").style.cssText=`
+                    transition: all ease-in-out 1s;
+                    height:100vh;
+                    font-size:33px;
+                    opacity:1;
+                `;
+                document.querySelector(".openMenu").addEventListener("click",()=>{
+                    document.querySelector(".openMenu").style.cssText=`
+                    transition: all ease-in-out 1s;
+                    height:0;
+                    font-size:0px;
+                    bottom:0;
+                    
+                     
+                `;
+                })
+            })
+        }
+        resolve(menu());
+    })
+};
 function first(){
     return new Promise((resolve)=>{
         function okFirst(){
@@ -36,6 +67,7 @@ function date(){
     });
 }
 async function main(){
+    await openMenu();
     await first();
     await second();
     await date();
